6a96112f7a18bbab945a99cd49e977b305c16939
shazidmashrafi <90213199+ShazidMashrafi@users.noreply.github.com>
2025-08-09 02:00:59 +0600

    update

---
+++ 11. Uppercase to Lowercase /code.asm
@@ -0,0 +1,24 @@
+.model
+.stack 100h
+.code
+main proc
+    mov ah,1 
+    int 21h
+    mov bl, al
+
+    mov ah, 2
+    mov dl, 10
+    int 21h
+    mov dl, 13
+    int 21h
+
+    mov ah, 2
+    add bl, 32
+    mov dl, bl
+    int 21h
+
+    exit:
+    mov ax, 4ch
+    int 21h
+    main endp
+end main
